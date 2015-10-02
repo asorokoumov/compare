e4046f4aebc2257d7a459854bfcc7003fb7915fd
Sorokoumov Anton <anton.sorokoumov@lamoda.ru>
2015-10-02 10:54:41 +0300

    remove whitenoise 2

--- compare/wsgi.py
+++ compare/wsgi.py
@@ -14,4 +14,3 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "compare.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
