d67f31a6adb05aeb1df36cc607bea4972c0ef04a
Sorokoumov Anton <anton.sorokoumov@lamoda.ru>
2015-10-02 10:50:39 +0300

    remove STATICFILES_DIRS

--- compare/settings.py
+++ compare/settings.py
@@ -106,10 +106,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
