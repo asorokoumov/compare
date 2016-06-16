3de9c4d6e6951e14c36b2439938d4a957ce8638b
Anton Sorokoumov <sorokoumov.anton@gmail.com>
2016-06-16 18:27:34 +0300

    One more attempt to fix log paths on production 2

--- compare/settings.py
+++ compare/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-LOGGING_ROOT = PROJECT_DIR
+LOGGING_ROOT = os.path.join(PROJECT_DIR, 'log')
 
 STATIC_URL = '/static/'
 
