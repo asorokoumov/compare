a3d9630b9bc6aa8076ab09c09d6a4ee699ced8a6
Anton Sorokoumov <sorokoumov.anton@gmail.com>
2016-07-08 13:10:39 +0300

    Fix next page url for Akusherstvo

--- diapers/utils/crutch.py
+++ diapers/utils/crutch.py
@@ -3,3 +3,10 @@ def item_url(url, seller):
         return url.split('?')[0]
     else:
         return url
+
+
+def next_url(url, seller):
+    if seller.name == "Akusherstvo":
+        return 'magaz.php' + url
+    else:
+        return url
