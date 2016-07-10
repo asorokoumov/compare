84d4da430271113d64b8e95896778031c07348df
Anton Sorokoumov <sorokoumov.anton@gmail.com>
2016-07-10 17:37:02 +0300

    fix ini path to production values

--- diapers/utils/parser.py
+++ diapers/utils/parser.py
@@ -9,8 +9,8 @@ import logging
 
 from configobj import ConfigObj
 
-shop_xpath = ConfigObj('diapers/utils/data_config/shop_xpath.ini')
-shop_urls = ConfigObj('diapers/utils/data_config/shop_urls.ini')
+shop_xpath = ConfigObj('compare/diapers/utils/data_config/shop_xpath.ini')
+shop_urls = ConfigObj('compare/diapers/utils/data_config/shop_urls.ini')
 
 
 __author__ = 'anton.sorokoumov'
