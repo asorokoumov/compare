6a2d42280e89bad26e7ae8edaf748da41fc571c4
Sorokoumov Anton <sorokoumov.anton@gmail.com>
2018-06-12 14:57:31 +0300

    Add correct page titles

--- diapers/urls.py
+++ diapers/urls.py
@@ -11,11 +11,11 @@ urlpatterns = [
     url(r'^admin/$', views.admin, name='admin'),
 
     # TODO create human understanding urls
-    url(r'^(?P<brand_id>[0-9]+)/$', views.show_series, name='series'),
-    url(r'^(?P<brand_id>[0-9]+)/(?P<series_id>[0-9]+)/$', views.show_sizes, name='sizes'),
-    url(r'^(?P<brand_id>[0-9]+)/(?P<series_id>[0-9]+)/(?P<size>.+)/$', views.show_products, name='products'),
+    # url(r'^(?P<brand_id>[0-9]+)/$', views.show_series, name='series'),
+    # url(r'^(?P<brand_id>[0-9]+)/(?P<series_id>[0-9]+)/$', views.show_sizes, name='sizes'),
+    # url(r'^(?P<brand_id>[0-9]+)/(?P<series_id>[0-9]+)/(?P<size>.+)/$', views.show_products, name='products'),
 
-
+    # admin
     url(r'^parse/update_product_list/$', views.update_product_list, name='update_product_list'),
     url(r'^parse/update_brand_list/$', views.update_brand_list, name='update_brand_list'),
     url(r'^parse/recreate/$', views.recreate, name='recreate'),
